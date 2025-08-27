9f9507285e92b61c5dbffa47ff1897d294dedb4f
LissEbalo <xlissx@gmail.com>
2025-08-27 16:42:50 -0400

    clase22_parte2

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+    # Ruta para el perfil del usuario
+    get "profile", to: "users#profile", as: :profile
+
+    # Ruta para probar emails
+    get "test/send_test_email", to: "test_mailer#send_test_email", as: :send_test_email
 end
